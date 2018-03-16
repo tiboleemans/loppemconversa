1adc0a19c0e0acec17984d0ff9e733296ba3da65
Thibault Leemans <tiboleemans@gmail.com>
2018-03-16 17:23:15 +0100

    try upload method

--- php/upload.php
+++ php/upload.php
@@ -19,11 +19,6 @@ if (file_exists($target_file)) {
     echo "Sorry, file already exists.";
     $uploadOk = 0;
 }
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
