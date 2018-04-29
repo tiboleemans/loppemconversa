c8001fc5c876d55e64d2a8c6ca92516a032d6a8d
Thibault Leemans <tiboleemans@gmail.com>
2018-04-29 12:29:36 +0200

    upload

--- php/upload.php
+++ php/upload.php
@@ -5,7 +5,7 @@ $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"][0]);
     if($check !== false) {
         echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
