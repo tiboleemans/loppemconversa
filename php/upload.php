549c1ea8dc1b75bacc478e4ebad6658be4d7efed
Thibault Leemans <tiboleemans@gmail.com>
2018-04-29 12:31:18 +0200

    upload

--- php/upload.php
+++ php/upload.php
@@ -1,6 +1,6 @@
 <?php
 $target_dir = "images/gallery/july2018";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"][0]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
@@ -31,8 +31,8 @@ if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"][0], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"][0]). " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
