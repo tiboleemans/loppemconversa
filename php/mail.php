f2c8dd7425ed6a5da37d425b4301f36b6bdd2b8d
Thibault Leemans <tiboleemans@gmail.com>
2018-06-22 22:03:49 +0200

    wachtlijst

--- php/mail.php
+++ php/mail.php
@@ -189,6 +189,7 @@ Loppem Conversa' . "\n\n" ;
 
 // simple email content
 if($_POST["pageLang"] == "NL"){
+	console.log('NL:' . $periodeInput);
 	if($periodeInput == "2-12 juli 2018"){
 		$email_subject_parent = 'Bevestiging wachtlijst Loppem Conversa';
 		$email_content_parent =  $email_content_introduction_wait_list_FR;
