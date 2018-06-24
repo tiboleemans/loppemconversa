d45d9e22dd87aed597224d583922b3fa287c9c2b
Thibault Leemans <tiboleemans@gmail.com>
2018-06-24 16:44:19 +0200

    gallery

---
+++ php/gallery.php
@@ -0,0 +1,15 @@
+<?php
+
+$_POST["langInput"];
+
+array(
+    "july2018" => "paterleon",
+    "august2018" => "paterleon",
+    "july2017" => "paterleon", 
+    "august2017" => "paterleon" 
+);
+
+$response_array['status'] = 'failed';
+echo json_encode($response_array);
+
+?>
